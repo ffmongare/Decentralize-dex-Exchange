bb39a25c4307358ff99d9bd8a53769dd2ba01f8b
ffmongare <frankmongare17@gmail.com>
2023-05-04 12:36:11 +0300

    Finalized the swap function using 1inch aggregator API

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -3,7 +3,10 @@ import Logo from '../blue.svg';
 import Eth from '../eth.svg';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+
   return (
     <div>
       <header>
@@ -23,9 +26,9 @@ function Header() {
             <img src={Eth} alt="eth" className='eth' />
             Ethereum
           </div>
-          <div className="connectButton">
-            Connect
-          </div>
+          <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
         </div>
       </header>
     </div>
