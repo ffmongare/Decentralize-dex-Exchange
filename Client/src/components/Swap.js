b55e13489f900e7e5ba17f232311d8048a545945
ffmongare <frankmongare17@gmail.com>
2023-05-04 09:28:38 +0300

    Finaliszed working on the backend to fetch prices using moralis Api and build the frontend logic for calculating the prices ratio between token

--- Client/src/components/Swap.js
+++ Client/src/components/Swap.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Input, Popover, Radio, Modal, message } from 'antd';
 import {ArrowDownOutlined, DownOutlined, SettingOutlined } from '@ant-design/icons';
 import tokenList from '../tokenList.json';
+import axios from 'axios';
 
 function Swap() {
 
@@ -12,20 +13,30 @@ function Swap() {
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
   const [isOpen, setIsOpen] = useState(false);
   const [changeToken, setChangeToken] = useState((1));
+  const [prices, setPrices] = useState(null);
 
   function handleSlippageChange(e) {
     setSlippage(e.target.value);
   }
 
   function changeAmount(e) {
-    setTokenOneAmount(e.target.value)
+    setTokenOneAmount(e.target.value);
+    if(e.target.value && prices) {
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2))
+    } else {
+      setTokenTwoAmount(null);
+    }
   }
 
   function switchTokens() {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
     const one = tokenOne;
     const two = tokenTwo;
     setTokenOne(two);
     setTokenTwo(one);
+    fetchPrices(two.address, one.address);
   }
 
   function openModal(asset) {
@@ -34,13 +45,31 @@ function Swap() {
   }
 
   function modifyToken(i) {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
     if(changeToken === 1) {
       setTokenOne(tokenList[i]);
+      fetchPrices(tokenList[i].address, tokenTwo.address)
     } else {
       setTokenTwo(tokenList[i]);
+      fetchPrices(tokenTwo.address, tokenList[i].address)
     }
     setIsOpen(false);
   }
+
+  async function fetchPrices(one, two) {
+    const res = await axios.get(`http://localhost:3001/tokenPrice`, {
+      params: {addressOne: one, addressTwo: two}
+    });
+
+    // console.log(res.data);
+    setPrices(res.data);
+  }
+
+  useEffect(() => {
+    fetchPrices(tokenList[0].address, tokenList[1].address)
+  }, [])
 
   const settings = (
     <>
@@ -94,7 +123,7 @@ function Swap() {
         </Popover>
       </div>
       <div className="inputs">
-        <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} />
+        <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} disabled={!prices} />
         <Input placeholder='0' value={tokenTwoAmount} disabled={true} />
         <div className="switchButton" onClick={switchTokens} >
           <ArrowDownOutlined className='switchArrow' />
